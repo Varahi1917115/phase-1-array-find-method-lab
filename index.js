fde232e37a091e2a104bbe976e6b4dadc671422e
Samarth <Varahi1917115@gmail.com>
2022-04-16 12:15:46 -0400

    commit tree 482abcb48665104c32868e68e19a3641e3b813ba

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+   
+function superbowlWin(recordList) {
+    const data = recordList.find(record => {
+       return record.result === 'W'
+
+       // console.log(record)
+   
+    });
+   console.log (data)
+    if (data) {
+    return data.year
+    }
+}
